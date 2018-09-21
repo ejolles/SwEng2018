ea690613c28ae509c2e5b2fa62e534171d07c566
ejolles <jolleseric@gmail.com>
2018-09-21 16:27:53 +0200

    ready to recerive travis

--- app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/sweng/bootcamp/MainActivityTest.java
@@ -1,5 +1,18 @@
 package ch.epfl.sweng.bootcamp;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
     @Rule
